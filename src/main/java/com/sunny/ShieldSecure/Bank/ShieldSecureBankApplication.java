e53858844e2b8dfd43aa874ca6aa83317dbf236d
Sunny <sunnysagar620@gmail.com>
2024-05-02 12:29:47 +0530

    Initial commit

---
+++ src/main/java/com/sunny/ShieldSecure/Bank/ShieldSecureBankApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.ShieldSecure.Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldSecureBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldSecureBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunny/ShieldSecure/Bank/ShieldSecureBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.ShieldSecure.Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldSecureBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
